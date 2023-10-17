4635092aba3f27ba99bd0947720ba3f7d16008cc
shimna <chimmu.p@gmail.com>
2023-10-17 15:53:38 +1100

    added icon in index.html and added code in index.js to require and export typeDefs and resolvers

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+//connects to mongoDB Atlas database if MONGODB_URI present else connects to local database
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/sparkshopper"
 );
