df7ba96b2442b4263291ec8292b34f25b47201da
sakshiarora04 <sakshiarora245@gmail.com>
2023-10-20 01:00:56 +1100

    comments add in code

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,12 +3,15 @@ const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
   Query: {
+    // get all categories
     categories: async () => {
       return await Category.find();
     },
-    subcategories:async(parent, { category})=>{
-      return await SubCategory.find(category);
+    // get subcategory of selected category
+    subcategories: async(parent, {category})=>{
+      return await SubCategory.find({category});
     },
+    // get all products of given subcategory
     products: async (parent, { subcategory, name }) => {
       try {
         const params = {};
@@ -22,12 +25,13 @@ const resolvers = {
             $regex: name,
           };
         }
-
+        // params can be name or subcategory id
         return await Product.find(params).populate("subcategory");
       } catch (error) {
         console.log(error);
       }
     },
+    // get one product of particular id
     product: async (parent, { _id }) => {
       try {
         return await Product.findById(_id).populate("subcategory");
@@ -35,14 +39,15 @@ const resolvers = {
         console.log("product not found", error);
       }
     },
+    // get user by id
     user: async (parent, args, context) => {
       if (context.user) {
         try {
           const user = await User.findById(context.user._id).populate({
             path: "orders.products",
-            populate: "category",
+            populate: "subcategory",
           });
-
+          //sorting as per latest purchase to oldest purchase date
           user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
 
           return user;
@@ -53,30 +58,34 @@ const resolvers = {
 
       throw AuthenticationError;
     },
-    order: async (parent, { _id }, context) => {
-      if (context.user) {
-        try {
-          const user = await User.findById(context.user._id).populate({
-            path: "orders.products",
-            populate: "category",
-          });
+    //
+    // order: async (parent, { _id }, context) => {
+    //   if (context.user) {
+    //     try {
+    //       const user = await User.findById(context.user._id).populate({
+    //         path: "orders.products",
+    //         populate: "category",
+    //       });
 
-          return user.orders.id(_id);
-        } catch (error) {
-          console.log("No orders found", error);
-        }
-      }
+    //       return user.orders.id(_id);
+    //     } catch (error) {
+    //       console.log("No orders found", error);
+    //     }
+    //   }
 
-      throw AuthenticationError;
-    },
+    //   throw AuthenticationError;
+    // },
+    
     checkout: async (parent, args, context) => {
       try {
+
         const url = new URL(context.headers.referer).origin;
+        // create new order
         const order = new Order({ products: args.products });
         const line_items = [];
-
+        //populate products of order
         const { products } = await order.populate("products");
-
+        //order information
         for (let i = 0; i < products.length; i++) {
           const product = await stripe.products.create({
             name: products[i].name,
@@ -95,7 +104,8 @@ const resolvers = {
             quantity: 1,
           });
         }
-
+        
+        //create and return checkout session
         const session = await stripe.checkout.sessions.create({
           payment_method_types: ["card"],
           line_items,
@@ -111,6 +121,7 @@ const resolvers = {
     },
   },
   Mutation: {
+    // add category by passing name
     addCategory: async (parent, { name }, context) => {
       if (context.user?.isAdmin) {
         try {
@@ -120,6 +131,7 @@ const resolvers = {
         }
       }
     },
+    //create sub category by passing name and category id
     addSubCategory: async (parent, { name,category }, context) => {
         if (context.user?.isAdmin) {
           try {
@@ -130,6 +142,7 @@ const resolvers = {
           }
         }
       },
+      //add products by passing all product details
       addProduct: async (parent, { productDetails }, context) => {
         if (context.user?.isAdmin) {
           try {
@@ -140,6 +153,7 @@ const resolvers = {
           }
         }
       },
+      //add comment in product 
       addComment: async (parent, {productId, rating, commentDesc,userId }) => {
         return Product.findOneAndUpdate(
           { _id: productId },
@@ -152,6 +166,7 @@ const resolvers = {
           }
         );
       },
+      //signup
     addUser: async (parent, args) => {
       try {
         const user = await User.create(args);
@@ -162,6 +177,7 @@ const resolvers = {
         console.log(error);
       }
     },
+    //add order in orders array and update  user
     addOrder: async (parent, { products }, context) => {
       if (context.user) {
         try {
@@ -179,19 +195,21 @@ const resolvers = {
 
       throw AuthenticationError;
     },
-    updateUser: async (parent, args, context) => {
-      if (context.user) {
-        try {
-          return await User.findByIdAndUpdate(context.user._id, args, {
-            new: true,
-          });
-        } catch (error) {
-          console.log("unable to update user", error);
-        }
-      }
+    // update user details
+    // updateUser: async (parent, args, context) => {
+    //   if (context.user) {
+    //     try {
+    //       return await User.findByIdAndUpdate(context.user._id, args, {
+    //         new: true,
+    //       });
+    //     } catch (error) {
+    //       console.log("unable to update user", error);
+    //     }
+    //   }
 
-      throw AuthenticationError;
-    },
+    //   throw AuthenticationError;
+    // },
+    //update category by passing id and name
     updateCategory: async (parent, {_id,name}, context) => {
         if (context.user?.isAdmin) {
           try {
@@ -205,6 +223,7 @@ const resolvers = {
   
         throw AuthenticationError;
       },
+    //update subcategory by passing id, name and category id
       updateSubCategory: async (parent, {_id,name,category}, context) => {
         if (context.user?.isAdmin) {
           try {
@@ -218,6 +237,7 @@ const resolvers = {
   
         throw AuthenticationError;
       },
+      //update quantity in product
     updateProduct: async (parent, { _id, quantity }) => {
       try {
         const decrement = Math.abs(quantity) * -1;
@@ -231,7 +251,7 @@ const resolvers = {
         console.log("unable to update product", error);
       }
     },
-    
+    // login
     login: async (parent, { email, password }) => {
       try {
         const user = await User.findOne({ email });
@@ -239,7 +259,7 @@ const resolvers = {
         if (!user) {
           throw AuthenticationError;
         }
-
+        // check password
         const correctPw = await user.isCorrectPassword(password);
 
         if (!correctPw) {
