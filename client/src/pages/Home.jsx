501c76f3f331db7efacd11f4cdef9faa79d590d9
shimna <chimmu.p@gmail.com>
2023-10-20 21:01:28 +1100

    updated the styles of card and wish list icon

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,18 +8,18 @@ import {
   CardHeader,
   keyframes,
   Box,
-  Icon,
   Text,
   Button,
   Image,
   chakra,
-  Heading,
   Center,
   Tooltip,
+  IconButton,
   Stack,
 } from "@chakra-ui/react";
 import { FaPlayCircle, FaRegHeart } from "react-icons/fa";
-
+// import { QUERY_PRODUCTS } from "../utils/queries";
+import { useQuery } from "@apollo/client";
 //Importing the required component for linking between pages
 import pic from "../assets/drone.webp";
 import { Link } from "react-router-dom";
@@ -34,9 +34,17 @@ const pulseRing = keyframes`
 }
 `;
 const Home = () => {
-  //  react hooks
-
   const [products, setProducts] = useState([]);
+  // const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+  // const productData = data?.products || {};
+
+  // useEffect(() => {
+  //   if (data) {
+  //     console.log(data);
+  //     setProducts(data.products);
+  //   }
+  // }, []);
 
   // When the component mounts to the VDOM, run this callback to retrieve the information about all projects
   useEffect(() => {
@@ -85,8 +93,8 @@ const Home = () => {
                   <Link href={product.github}>
                     <Box
                       as="div"
-                      bgGradient="linear(to-r, gray.300, orange.100, yellow.400)"
-                      align={"end"}
+                      // bgGradient="linear(to-r, gray.300, orange.100, yellow.400)"
+                      align={"right"}
                       mr={10}
                     >
                       <Tooltip
@@ -97,110 +105,115 @@ const Home = () => {
                         fontSize={"1.2em"}
                       >
                         <chakra.a href={"#"} display={"flex"}>
-                          <Icon
-                            position="absolute"
-                            as={FaRegHeart}
-                            boxSize={{ base: "24px", md: "24px" }}
+                          <IconButton
+                            isRound={true}
+                            variant="solid"
+                            colorScheme="gray"
+                            aria-label="Done"
+                            fontSize="20px"
+                            icon={<FaRegHeart />}
                             _hover={{
                               color: "red.600",
+                              fontSize: { base: "20px", md: "24px" },
                             }}
-                            _click={{ color: "red.600",}}
                           />
                         </chakra.a>
                       </Tooltip>
                     </Box>
                   </Link>
                 </CardHeader>
-                <Center py={12}>
-                  <Box
-                    role={"group"}
-                    p={6}
-                    maxW={"330px"}
-                    w={"full"}
-                    bg={"white"}
-                    boxShadow={"2xl"}
-                    rounded={"lg"}
-                    pos={"relative"}
-                    zIndex={1}
-                  >
-                    <Link href={product.href}>
-                      <Box
-                        rounded={"lg"}
-                        mt={-12}
-                        pos={"relative"}
-                        height={"230px"}
-                        _after={{
-                          transition: "all .3s ease",
-                          content: '""',
-                          w: "full",
-                          h: "full",
-                          pos: "absolute",
-                          top: 5,
-                          left: 0,
-                          backgroundImage: `url(${pic})`,
-                          filter: "blur(15px)",
-                          zIndex: -1,
-                        }}
-                        _groupHover={{
-                          _after: {
-                            filter: "blur(20px)",
-                          },
-                        }}
-                      >
-                        <Image
-                          rounded={"lg"}
-                          // height={230}
-                          // width={282}
-                          objectFit={"cover"}
-                          src={pic}
-                          alt="#"
-                        />
-                      </Box>
-                      <Stack pt={10} align={"center"}>
-                        <Text
-                          fontSize={"md"}
-                          pb={"10px"}
-                          align={"center"}
-                          mb={10}
-                          fontWeight={700}
-                        >
-                          Panasonic LUMIX S5II Full-Frame Mirrorless Camera with
-                          20-60mm Lens [6K Video]
-                        </Text>
+                <CardBody>
+                  <Center py={12}>
+                    <Box
+                      role={"group"}
+                      p={6}
+                      maxW={"330px"}
+                      w={"full"}
+                      bg={"white"}
+                      boxShadow={"2xl"}
+                      rounded={"lg"}
+                      pos={"relative"}
+                      zIndex={1}
+                    >
+                      <Link href={product.href}>
                         <Box
-                          mt={1}
-                          p={2}
-                          bgGradient="linear(to-r, gray.300, orange.100, orange.100)"
-                          align={"center"}
-                          borderTopLeftRadius={40}
-                          borderBottomRightRadius={40}
-                          width="150px"
-                          fontWeight={700}
-                        >
-                          $ 1999
-                        </Box>
-                        <Button
-                          mt={10}
-                          p={2}
-                          colorScheme="black"
-                          type="submit"
-                          bgGradient={["linear(to-b, blue.700, gray.800)"]}
-                          borderRadius="full"
-                          boxShadow={
-                            "0px 1px 25px -5px (blue.700 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
-                          }
-                          _hover={{
-                            bg: "gray.700",
+                          rounded={"lg"}
+                          mt={-12}
+                          pos={"relative"}
+                          height={"230px"}
+                          _after={{
+                            transition: "all .3s ease",
+                            content: '""',
+                            w: "full",
+                            h: "full",
+                            pos: "absolute",
+                            top: 5,
+                            left: 0,
+                            backgroundImage: `url(${pic})`,
+                            filter: "blur(15px)",
+                            zIndex: -1,
                           }}
-                          width="250px"
-                          align={"center"}
+                          _groupHover={{
+                            _after: {
+                              filter: "blur(20px)",
+                            },
+                          }}
                         >
-                          Add To Cart
-                        </Button>
-                      </Stack>
-                    </Link>
-                  </Box>
-                </Center>
+                          <Image
+                            rounded={"lg"}
+                            // height={230}
+                            // width={282}
+                            objectFit={"cover"}
+                            src={pic}
+                            alt="#"
+                          />
+                        </Box>
+                        <Stack pt={10} align={"center"}>
+                          <Text
+                            fontSize={"md"}
+                            pb={"10px"}
+                            align={"center"}
+                            mb={10}
+                            fontWeight={700}
+                          >
+                            Panasonic LUMIX S5II Full-Frame Mirrorless Camera
+                            with 20-60mm Lens [6K Video]
+                          </Text>
+                          <Box
+                            mt={1}
+                            p={2}
+                            bgGradient="linear(to-r, gray.300, orange.100, orange.100)"
+                            align={"center"}
+                            borderTopLeftRadius={40}
+                            borderBottomRightRadius={40}
+                            width="150px"
+                            fontWeight={700}
+                          >
+                            $ 1999
+                          </Box>
+                          <Button
+                            mt={10}
+                            p={2}
+                            colorScheme="black"
+                            type="submit"
+                            bgGradient={["linear(to-b, blue.700, gray.800)"]}
+                            borderRadius="full"
+                            boxShadow={
+                              "0px 1px 25px -5px (blue.700 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
+                            }
+                            _hover={{
+                              bg: "gray.700",
+                            }}
+                            width="250px"
+                            align={"center"}
+                          >
+                            Add To Cart
+                          </Button>
+                        </Stack>
+                      </Link>
+                    </Box>
+                  </Center>
+                </CardBody>
               </Card>
             </GridItem>
           ))}
