2f0c6f58d7475e22852940fd261b078c2b3c620c
shimna <chimmu.p@gmail.com>
2023-10-21 12:21:19 +1100

    added actions and reducers for updating the products in state

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,17 @@
+import { UPDATE_PRODUCTS } from "./actions";
+
+// The reducer is a function that accepts the current state and an action. It returns a new state based on that action.
+export const reducer = (state, action) => {
+  switch (action.type) {
+    // Returns a copy of state with an update products array. We use the action.products property and spread it's contents into the new array.
+    case UPDATE_PRODUCTS:
+      return {
+        ...state,
+        products: [...action.products],
+      };
+    // Return the state as is in the event that the `action.type` passed to our reducer was not accounted for by the developers
+    // This saves us from a crash.
+    default:
+      return state;
+  }
+};
