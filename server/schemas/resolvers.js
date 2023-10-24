082a86091d82d4b230bc9d0410cd41ac02ab7413
Tristenh <tristenhobba1@gmail.com>
2023-10-24 18:52:50 +1100

    added stripe required package

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { User, Product, Category, Order, Comment,SubCategory } = require("../models");
 const { signToken, AuthenticationError } = require("../utils/auth");
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
   Query: {
