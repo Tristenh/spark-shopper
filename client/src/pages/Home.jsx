ce42dc6b9501fac3da7447f887f203b00a43696f
shimna <chimmu.p@gmail.com>
2023-10-27 23:41:58 +1100

    updated code in home page to clear states on load

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,16 @@
 //import components cart and ProductList
 import Cart from "../components/Cart";
 import ProductList from "../components/ProductList";
+import { CLEAR_STATES } from "../utils/actions";
+//importing actions,queries, GlobalState and helpers
+import { useStoreContext } from "../utils/GlobalState";
+import { useEffect } from "react";
 const Home = () => {
+  const [state, dispatch] = useStoreContext();
+  useEffect(() => {
+    //clears the states
+    dispatch({ type: CLEAR_STATES });
+  }, [dispatch]);
   //renders products on home page
   return (
     <>
