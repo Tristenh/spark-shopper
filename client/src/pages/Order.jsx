2ac887550d91c3167da9abfc4c4cd398714127a8
Tristenh <tristenhobba1@gmail.com>
2023-10-29 20:46:44 +1100

    update Chakra styling on Order.jsx

--- client/src/pages/Order.jsx
+++ client/src/pages/Order.jsx
@@ -4,16 +4,12 @@ import { useQuery } from "@apollo/client";
 import { QUERY_USER } from "../utils/queries";
 import {
   Grid,
-  Spinner,
-  Heading,
   Flex,
   VStack,
-  Divider,
-  Center,
   Box,
   Card,
   CardBody,
-  Text
+  Text,
 } from "@chakra-ui/react";
 
 function Order() {
@@ -30,93 +26,92 @@ function Order() {
   return (
     <Flex justify={"center"} mt={50}>
       {user ? (
-        <>
-          <VStack>
-            <Link to="/" color="black">
-              ← Back to Products
-            </Link>
-            <h2>Order History for {user.username}</h2>
-
-            <Grid
-              templateRows={{
-                base: "repeat(1, 1fr)",
-                md: "repeat(2, 1fr)",
-                lg: "repeat(3, 1fr)",
-              }}
-              templateColumns={{
-                base: "repeat(1, 1fr)",
-                md: "repeat(2, 1fr)",
-                lg: "repeat(3, 1fr)",
-              }}
-            >
-              {user.orders.map((order) => (
-                <Card key={order._id} p={{ base: 0, md: 5 }} h={700}>
-                  <CardBody>
-                    {new Date(
-                      parseInt(order.purchaseDate)
-                    ).toLocaleDateString()}
-                  {order.products.map(({ _id, image, name, price }) => (
-                    <>
-                      <Link to={`/products/${_id}`}>
-                      <Box
-              role={"group"}
-              p={6}
-              maxW={"330px"}
-              w={"full"}
-              bg={"white"}
-              boxShadow={"2xl"}
-              rounded={"lg"}
-              pos={"relative"}
-              zIndex={1}
-              h={500}
+        <VStack>
+          <Link to="/" color="black">
+            ← Back to Products
+          </Link>
+          <h2>Order History for {user.username}</h2>
+          {user.orders.map((order) => (
+            <div key={order._id}>
+              <h3>
+                {" "}
+                {new Date(parseInt(order.purchaseDate)).toLocaleDateString()}
+              </h3>
+                <Card>
+              <Grid
+                templateColumns={{
+                  base: "repeat(1, 1fr)",
+                  md: "repeat(2, 1fr)",
+                  lg: "repeat(3, 1fr)",
+                }}
+                gap={4}
               >
-              <img alt={name} src={`/images/${image}`} />
-              <Text pt={10}
-                  fontSize={"md"}
-                  pb={"10px"}
-                  align={"center"}
-                  fontWeight={700}>
-              <p>{name}</p>
-        <div>
-          <span>${price}</span>
-        </div>
-
-              </Text>
-              </Box>
-                        <Box
-                          rounded={"lg"}
-                          mt={-20}
-                          pos={"relative"}
-                          height={"230px"}
-                          _after={{
-                            transition: "all .3s ease",
-                            content: '""',
-                            w: "full",
-                            h: "full",
-                            pos: "absolute",
-                            top: 5,
-                            left: 0,
-        
-                            filter: "blur(15px)",
-                            zIndex: -1,
-                          }}
-                          _groupHover={{
-                            _after: {
-                              filter: "blur(20px)",
-                            },
-                          }}
+                  
+                {order.products.map(({ _id, image, name, price }) => (
+                    <Card key={_id} p={{ base: 0, md: 5 }}>
+                      <CardBody>
+                        <Link to={`/products/${_id}`}>
+                          <Box
+                            role={"group"}
+                            p={6}
+                            maxW={"330px"}
+                            w={"full"}
+                            bg={"white"}
+                            boxShadow={"2xl"}
+                            rounded={"lg"}
+                            pos={"relative"}
+                            zIndex={1}
+                            h={500}
                           >
+                            <img alt={name} src={`/images/${image}`} />
+                            <Text
+                              pt={10}
+                              fontSize={"md"}
+                              pb={"10px"}
+                              align={"center"}
+                              fontWeight={700}
+                            >
+                              <p>{name}</p>
+                              <div>
+                                <span>${price}</span>
+                              </div>
+                            </Text>
+                          </Box>
+                          <Box
+                            rounded={"lg"}
+                            mt={-20}
+                            pos={"relative"}
+                            height={"230px"}
+                            _after={{
+                              transition: "all .3s ease",
+                              content: '""',
+                              w: "full",
+                              h: "full",
+                              pos: "absolute",
+                              top: 5,
+                              left: 0,
+                              filter: "blur(15px)",
+                              zIndex: -1,
+                            }}
+                            _groupHover={{
+                              _after: {
+                                filter: "blur(20px)",
+                              },
+                            }}
+                          ></Box>
+                        </Link>
+                      </CardBody>
+                    </Card>
+                ))}
+              </Grid>
+          </Card>
+            </div>
+          )
 
-                        </Box>
-                      </Link>
-                    </>
-                  ))}
-                  </CardBody>
-                </Card>
-              ))}
-            </Grid>
-          </VStack>
-        </>
+          )
+        
+        }
+        </VStack>
       ) : null}
     </Flex>
   );
