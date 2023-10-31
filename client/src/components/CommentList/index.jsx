4ce1f6415fe125f368913c8c71f6ec5ce9ac3662
sakshiarora04 <sakshiarora245@gmail.com>
2023-10-31 13:12:28 +1100

    delete button alignment

--- client/src/components/CommentList/index.jsx
+++ client/src/components/CommentList/index.jsx
@@ -1,19 +1,19 @@
 // import chakra components
 import { Stack, Text, Box, VStack, Flex, HStack } from "@chakra-ui/react";
 // import react icon
-import { AiFillStar, AiFillDelete } from "react-icons/ai";
+import { AiFillStar } from "react-icons/ai";
+import { RiDeleteBinLine } from "react-icons/ri";
 import { idbPromise } from "../../utils/helpers";
 
 import { useStoreContext } from "../../utils/GlobalState";
 import { useMutation } from "@apollo/client";
 import { REMOVE_COMMENT } from "../../utils/mutations";
-import{CURRENT_PRODUCT} from "../../utils/actions"
+import { CURRENT_PRODUCT } from "../../utils/actions";
 const CommentList = () => {
   const [state, dispatch] = useStoreContext();
   const { currentProduct } = state;
   const [removeComment, { loading }] = useMutation(REMOVE_COMMENT);
 
- 
   const removeCommentbyId = async (commentId) => {
     const { data } = await removeComment({
       variables: {
@@ -21,18 +21,17 @@ const CommentList = () => {
         productId: currentProduct._id,
       },
     });
-   
-    if(data){
+
+    if (data) {
       dispatch({
         type: CURRENT_PRODUCT,
-        currentProduct:{...data.removeComment},
-      
+        currentProduct: { ...data.removeComment },
       });
       console.log(currentProduct.comments);
       idbPromise("singleProduct", "put", data.removeComment);
     } else if (!loading) {
       idbPromise("singleProduct", "get").then((indexedProduct) => {
-        console.log(indexedProduct)
+        console.log(indexedProduct);
         dispatch({
           type: CURRENT_PRODUCT,
           product: indexedProduct,
@@ -47,7 +46,7 @@ const CommentList = () => {
           <Text fontSize={{ base: "lg", lg: "xl" }}>Customer Reviews</Text>
         </Box>
       </Stack>
-      <VStack alignItems={"flex-start"}>
+      <VStack>
         {/* {console.log(state.currentProduct,state.comments)} */}
         {currentProduct.comments &&
           currentProduct.comments.map((comment) => (
@@ -65,15 +64,22 @@ const CommentList = () => {
               {/* displays user name, comment text, rating in numbers and date of comment */}
               <Box p={4}>
                 <Stack direction={"column"} spacing={0}>
-                  <Text fontSize={"md"} fontWeight={600}>
+                  <Text fontSize={"lg"} fontWeight={600}>
                     {comment.userName}{" "}
                   </Text>
-                  <Text fontSize={"xs"} color={"gray.500"}>
-                    {comment.dateCreated}                    
-                  </Text>
-                  <Box onClick={() => removeCommentbyId(comment._id)}>
-                      <AiFillDelete />
-                    </Box>
+                  <Flex justifyContent={"space-between"}>
+                    <Text fontSize={"sm"} color={"gray.500"}>
+                      {comment.dateCreated}
+                    </Text>
+                    <Text
+                      mt={0.5}
+                      ml={1}
+                      fontSize={"xl"}
+                      onClick={() => removeCommentbyId(comment._id)}
+                    >
+                      <RiDeleteBinLine />
+                    </Text>{" "}
+                  </Flex>
                 </Stack>
               </Box>
               <HStack borderTop={"1px"} borderColor="gray.400" color="black">
